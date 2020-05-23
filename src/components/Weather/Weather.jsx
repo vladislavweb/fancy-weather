d508e6d87c194aa7414dd1cd7f4ef679ebdae027
Vladislav Ivanov <himametsu@gmail.com>
2020-05-23 21:33:47 +0300

    https

--- src/components/Weather/Weather.jsx
+++ src/components/Weather/Weather.jsx
@@ -11,7 +11,7 @@ const link = 'https://api.openweathermap.org/data/2.5/forecast?';
 const keyWeather = 'eac54b3672659f3dd10f71d884f30279';
 const tokenMap = 'pk.eyJ1IjoiaGltaW1ldHN1IiwiYSI6ImNrYWNtZ3VheDBuc3gyc284djVrOW50MnUifQ.CKQQ3zFcMaaQWHB-vZ8KLQ';
 const tokenGeo = 'BtHcuGO81EUArGaV164zvKD5sTuERK2O'
-const urlGeo = 'http://www.mapquestapi.com/geocoding/v1/address?key='
+const urlGeo = 'https://www.mapquestapi.com/geocoding/v1/address?key='
 
 const Weather = props => {
   const { searchString, changeSearchString } = useContext(MainContext);
