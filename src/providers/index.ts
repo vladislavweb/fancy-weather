a461873992687332746621e9724a6cd24a21538c
Vlad Ivanov <himametsu@gmail.com>
2022-11-14 21:15:59 +0800

    update exports

--- src/providers/index.ts
+++ src/providers/index.ts
@@ -2,3 +2,4 @@ export { ConfigProvider, ConfigContext } from "./config";
 export { BackgroundProvider, BackgroundContext } from "./background";
 export { SettingsProvider, SettingsContext } from "./settings";
 export { WeatherProvider, WeatherContext } from "./weather";
+export { MapQuestProvider, MapQuestContext } from "./mapQuest";
