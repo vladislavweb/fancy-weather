ec4e0eb985f5375aaccd2a452d286833d8ff6f4d
Vlad Ivanov <himametsu@gmail.com>
2022-11-14 21:17:59 +0800

    refactor containers

--- src/containers/header/header.tsx
+++ src/containers/header/header.tsx
@@ -1,8 +1,9 @@
+import { FC } from "react";
 import ControlPanel from "../../components/controlPanel";
 import SearchPanel from "../../components/searchPanel";
 import "./header.css";
 
-const Header = () => (
+const Header: FC = () => (
   <header className="header">
     <ControlPanel />
     <SearchPanel />
--- src/containers/main/main.tsx
+++ src/containers/main/main.tsx
@@ -1,8 +1,9 @@
+import { FC } from "react";
 import Weather from "../../components/weather";
 import Map from "../../components/map";
 import "./main.css";
 
-const Main = () => {
+const Main: FC = () => {
   return (
     <main className="main">
       <Weather />
