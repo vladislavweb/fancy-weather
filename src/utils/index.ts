32acce6a770483f29cca85da5ec26ab75e7f3505
Vlad Ivanov <himametsu@gmail.com>
2022-12-16 05:24:52 +0800

    add utility to set background

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,5 +2,6 @@ export { getWeatherImage } from "./getWeatherImage";
 export { weatherMapper } from "./weatherMapper";
 export { mapBoxMapper } from "./mapBoxMapper";
 export { createWeatherDescription } from "./createWeatherDescription";
+export { setBackgroundImage } from "./setBackgroundImage";
 
 export type { WeatherMap, WeatherNow, WeatherThreeDays } from "./weatherMapper";
---
+++ src/utils/setBackgroundImage.ts
@@ -0,0 +1,36 @@
+import { BackgroundResponse } from "../types";
+
+interface Params {
+  imageData: BackgroundResponse;
+  onStart?: () => void;
+  onEnd?: () => void;
+}
+
+type SetBackgroundImage = ({}: Params) => Promise<void>;
+
+export const setBackgroundImage: SetBackgroundImage = async ({ imageData, onStart, onEnd }) => {
+  try {
+    return await new Promise<void>((res, rej) => {
+      if (onStart) {
+        onStart();
+      }
+
+      const image = new Image();
+      image.src = imageData.urls.regular;
+
+      image.onload = () => {
+        document.body.setAttribute("style", `background-image: url(${imageData.urls.regular})`);
+
+        res();
+      };
+
+      image.onerror = () => {
+        rej();
+      };
+    });
+  } finally {
+    if (onEnd) {
+      onEnd();
+    }
+  }
+};
